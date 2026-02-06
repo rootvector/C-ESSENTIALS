6272727b83b063252100c44d3dbf099137cb1e8d
rootvector <rootvector.sec@gmail.com>
2026-02-06 09:47:21 +0530

    Demo: C struct with first program

---
+++ resources/hello.c
@@ -0,0 +1,7 @@
+// C LANGUAGE PROGRAM STRUCTURE
+#include <stdio.h>    //HEADER FILES
+
+int main(){      // ENTRY POINT main() Function		-|
+	printf("Hello, World!");	//STATEMENT	-|	main() body	
+	return 0;			// RETURN VALUE  |
+}					//END OF MAIN() -|
